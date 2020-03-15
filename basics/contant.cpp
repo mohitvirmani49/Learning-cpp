c906d331243ea6e0ed0a6e5f4684947ccc838d42
Mohit Virmani <62204767+mohitvirmani49@users.noreply.github.com>
2020-03-16 01:18:16 +0530

    Create contant.cpp

---
+++ basics/contant.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    const int value = 50;  // Constant means the value of the variable cannot be changed in the whole program 
+    cout << "Value is " << value << "\n";
+    
+}
